53bee5b8230bbee6e5b3d93c97ce5c55dd227839
Jijo A J <love4messi@gmail.com>
2022-10-08 20:18:51 +0530

    getCountryList
    method getCountryList

--- src/main/java/com/jijo/jerseyhouse/controller/MainControllerInterface.java
+++ src/main/java/com/jijo/jerseyhouse/controller/MainControllerInterface.java
@@ -1,13 +1,19 @@
 package com.jijo.jerseyhouse.controller;
 
+import com.jijo.jerseyhouse.model.Country;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("main")
 public interface MainControllerInterface {
 
     @GetMapping("connect")
     ResponseEntity<String> connect();
+
+    @GetMapping("getCountryList")
+    ResponseEntity<List<Country>> getCountryList();
 
 }
--- src/main/java/com/jijo/jerseyhouse/controller/impl/MainController.java
+++ src/main/java/com/jijo/jerseyhouse/controller/impl/MainController.java
@@ -1,14 +1,32 @@
 package com.jijo.jerseyhouse.controller.impl;
 
 import com.jijo.jerseyhouse.controller.MainControllerInterface;
+import com.jijo.jerseyhouse.model.Country;
+import com.jijo.jerseyhouse.service.DeliveryServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class MainController implements MainControllerInterface {
+
+    @Autowired
+    DeliveryServiceInterface deliveryService;
+
     @Override
     public ResponseEntity<String> connect() {
         return new ResponseEntity<>("Successfully connected to Jersey house !",HttpStatus.OK);
+    }
+
+    /**
+     * method getCountryList
+     * @return List of countries
+     */
+    @Override
+    public ResponseEntity<List<Country>> getCountryList() {
+        return new ResponseEntity<>(deliveryService.getCountryList(),HttpStatus.OK);
     }
 }
