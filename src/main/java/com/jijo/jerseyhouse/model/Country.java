a156e3f1104ef323725457855cf096f78c723776
Jijo A J <love4messi@gmail.com>
2022-10-08 20:18:07 +0530

    Create Country.java
    Country Table Entity

---
+++ src/main/java/com/jijo/jerseyhouse/model/Country.java
@@ -0,0 +1,23 @@
+package com.jijo.jerseyhouse.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@Table(name = "COUNTRY")
+@Entity
+@Getter
+public class Country {
+    @Column(nullable = false)
+    @Id
+    private int countryCode;
+
+    @Column(nullable = false, length = 20)
+    private String countryName;
+}
