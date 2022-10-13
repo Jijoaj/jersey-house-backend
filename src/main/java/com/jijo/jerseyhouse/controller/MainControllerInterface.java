03e8f2df27a208922b1c1353b210ec58c5e99063
Jijo A J <love4messi@gmail.com>
2022-10-13 14:16:28 +0530

    Update MainControllerInterface.java

--- src/main/java/com/jijo/jerseyhouse/controller/MainControllerInterface.java
+++ src/main/java/com/jijo/jerseyhouse/controller/MainControllerInterface.java
@@ -1,9 +1,11 @@
 package com.jijo.jerseyhouse.controller;
 
 import com.jijo.jerseyhouse.model.Country;
+import com.jijo.jerseyhouse.model.League;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,5 +17,8 @@ public interface MainControllerInterface {
 
     @GetMapping("getCountryList")
     ResponseEntity<List<Country>> getCountryList();
+
+    @GetMapping("getLeagueByCountry")
+    ResponseEntity<List<League>> getLeagueByCountry(@RequestParam String country);
 
 }
