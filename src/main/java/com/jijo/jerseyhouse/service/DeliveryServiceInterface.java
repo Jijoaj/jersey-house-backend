d0d524b98b5ed4bd609798775153270cff78ae58
Jijo A J <love4messi@gmail.com>
2022-10-13 14:16:22 +0530

    Update DeliveryServiceInterface.java

--- src/main/java/com/jijo/jerseyhouse/service/DeliveryServiceInterface.java
+++ src/main/java/com/jijo/jerseyhouse/service/DeliveryServiceInterface.java
@@ -1,9 +1,12 @@
 package com.jijo.jerseyhouse.service;
 
 import com.jijo.jerseyhouse.model.Country;
+import com.jijo.jerseyhouse.model.League;
 
 import java.util.List;
 
 public interface DeliveryServiceInterface {
     List<Country> getCountryList();
+
+    List<League> getLeagueByCountry(String country);
 }
