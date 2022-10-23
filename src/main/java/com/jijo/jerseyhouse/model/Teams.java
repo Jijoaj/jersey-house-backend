b868161949e99394ae43a2cb04aa1a4959c2dcb9
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-10-23 16:35:47 +0530

    Update Teams.java

--- src/main/java/com/jijo/jerseyhouse/model/Teams.java
+++ src/main/java/com/jijo/jerseyhouse/model/Teams.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
