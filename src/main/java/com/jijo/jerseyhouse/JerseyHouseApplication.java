f46784e5b64e1c5542e241fb385516b8bd3181a3
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-10-24 15:52:53 +0530

    Update JerseyHouseApplication.java

--- src/main/java/com/jijo/jerseyhouse/JerseyHouseApplication.java
+++ src/main/java/com/jijo/jerseyhouse/JerseyHouseApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JerseyHouseApplication {
+	private JerseyHouseApplication() {
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JerseyHouseApplication.class, args);
