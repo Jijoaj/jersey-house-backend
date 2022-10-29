11bcad23555f2cfda49f1e7022d6bc4c346d4262
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-10-29 13:14:11 +0530

    Update V13__JERSEY_DDL.sql

--- src/main/resources/db/migration/V13__JERSEY_DDL.sql
+++ src/main/resources/db/migration/V13__JERSEY_DDL.sql
@@ -2,12 +2,17 @@ CREATE SEQUENCE JERSEY_SEQ START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE jersey
 (
-    id        INT NOT NULL,
-    team_code INT,
-    size      INT,
-    stock     INT,
+    id          INT NOT NULL,
+    team_code   INT,
+    size        INT,
+    stock       INT,
+    season_code INT,
+    image_url   VARCHAR(255),
     CONSTRAINT pk_jersey PRIMARY KEY (id)
 );
 
 ALTER TABLE jersey
-    ADD CONSTRAINT FK_JERSEY_ON_TEAM_CODE FOREIGN KEY (team_code) REFERENCES TEAMS (team_id);
+    ADD CONSTRAINT FK_JERSEY_ON_SEASON_CODE FOREIGN KEY (season_code) REFERENCES season (season_code);
+
+ALTER TABLE jersey
+    ADD CONSTRAINT FK_JERSEY_ON_TEAM_CODE FOREIGN KEY (team_code) REFERENCES teams (team_id);
