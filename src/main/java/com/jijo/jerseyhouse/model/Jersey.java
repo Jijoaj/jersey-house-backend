a59568257e9b3b0dc12f81b76e66dd9f7af21055
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-10-30 23:32:21 +0530

    Update Jersey.java

--- src/main/java/com/jijo/jerseyhouse/model/Jersey.java
+++ src/main/java/com/jijo/jerseyhouse/model/Jersey.java
@@ -1,12 +1,18 @@
 package com.jijo.jerseyhouse.model;
 
 import com.jijo.jerseyhouse.model.enums.TShirtSize;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "jersey")
 public class Jersey {
     @Id
@@ -14,14 +20,15 @@ public class Jersey {
     @SequenceGenerator(name = "jersey_seq",initialValue = 1,allocationSize = 1)
     @Column(name = "id", nullable = false)
     private Integer id;
-
     @ManyToOne
-    @JoinColumn(name = "team_code")
+    @JoinColumn(name = "team_code",  nullable = false)
     private Teams teamCode;
+    @Column(nullable = false)
     private TShirtSize size;
+    @Column(nullable = false)
     private Integer stock;
     @ManyToOne
-    @JoinColumn(name = "season_code")
+    @JoinColumn(name = "season_code", nullable = false)
     private Season seasonCode;
     private String imageUrl;
 }
