ac64c18bda797f62622ebdf56040f07a97488110
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-11-05 17:19:43 +0530

    Real madrid 21

--- src/main/resources/db/migration/V14__INSERT_JERSEY.sql
+++ src/main/resources/db/migration/V14__INSERT_JERSEY.sql
@@ -4,3 +4,10 @@ INSERT INTO jersey (id, team_code, size, stock, season_code, image_url) VALUES (
 INSERT INTO jersey (id, team_code, size, stock, season_code, image_url) VALUES (NEXTVAL('JERSEY_SEQ'), 1, 'l', 10, 20, 'https://images.app.goo.gl/oy3YMSNKNb7t3env9');
 INSERT INTO jersey (id, team_code, size, stock, season_code, image_url) VALUES (NEXTVAL('JERSEY_SEQ'), 1, 'xl', 10, 20, 'https://images.app.goo.gl/oy3YMSNKNb7t3env9');
 INSERT INTO jersey (id, team_code, size, stock, season_code, image_url) VALUES (NEXTVAL('JERSEY_SEQ'), 1, 'xs', 10, 20, 'https://images.app.goo.gl/oy3YMSNKNb7t3env9');
+
+-- Real Madrid 21
+INSERT INTO jersey (id, team_code, size, stock, season_code, image_url) VALUES (NEXTVAL('JERSEY_SEQ'), 1, 's', 10, 21, 'https://images.app.goo.gl/LzxLadqmXmgooGMM9');
+INSERT INTO jersey (id, team_code, size, stock, season_code, image_url) VALUES (NEXTVAL('JERSEY_SEQ'), 1, 'm', 10, 21, 'https://images.app.goo.gl/LzxLadqmXmgooGMM9');
+INSERT INTO jersey (id, team_code, size, stock, season_code, image_url) VALUES (NEXTVAL('JERSEY_SEQ'), 1, 'l', 10, 21, 'https://images.app.goo.gl/LzxLadqmXmgooGMM9');
+INSERT INTO jersey (id, team_code, size, stock, season_code, image_url) VALUES (NEXTVAL('JERSEY_SEQ'), 1, 'xl', 10, 21, 'https://images.app.goo.gl/LzxLadqmXmgooGMM9');
+INSERT INTO jersey (id, team_code, size, stock, season_code, image_url) VALUES (NEXTVAL('JERSEY_SEQ'), 1, 'xs', 10, 21, 'https://images.app.goo.gl/LzxLadqmXmgooGMM9');
