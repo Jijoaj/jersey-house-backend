e3b97ba7580d60432fb0152b4bd6f62b22a9bfa5
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-11-05 18:05:35 +0530

    Update ProductServiceInterface.java

--- src/main/java/com/jijo/jerseyhouse/service/ProductServiceInterface.java
+++ src/main/java/com/jijo/jerseyhouse/service/ProductServiceInterface.java
@@ -1,8 +1,12 @@
 package com.jijo.jerseyhouse.service;
 
+import com.jijo.jerseyhouse.model.Jersey;
 import com.jijo.jerseyhouse.model.Teams;
+import com.jijo.jerseyhouse.model.requests.JerseyRequest;
 
 import java.util.List;
 public interface ProductServiceInterface {
     List<Teams> getTeamsByLeagues(List<Integer> leagueCodeList);
+
+    List<Jersey> getJerseyView(JerseyRequest jerseyRequest);
 }
