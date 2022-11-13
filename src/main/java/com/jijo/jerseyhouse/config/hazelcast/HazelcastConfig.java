fde162da558363884a7028d7b6e32f3a726c3ae5
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-11-13 13:26:14 +0530

    Create HazelcastConfig.java

---
+++ src/main/java/com/jijo/jerseyhouse/config/hazelcast/HazelcastConfig.java
@@ -0,0 +1,26 @@
+package com.jijo.jerseyhouse.config.hazelcast;
+
+import com.hazelcast.config.*;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class HazelcastConfig {
+
+    @Bean
+    public Config hazelcastConfiguration(){
+        return new Config()
+                .setInstanceName("hazelcast-instance")
+                .addMapConfig(new MapConfig()
+                        .setName("jersey-house-cache")
+                        .setTimeToLiveSeconds(3000)
+                        .setEvictionConfig(new EvictionConfig()
+                                .setSize(200)
+                                .setMaxSizePolicy(MaxSizePolicy.FREE_HEAP_SIZE)
+                                .setEvictionPolicy(EvictionPolicy.LRU)
+                ));
+
+    }
+}
