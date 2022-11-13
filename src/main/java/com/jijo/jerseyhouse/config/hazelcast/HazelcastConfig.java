3dd1ff1ecd0d12fa3d615f7a6dcbf8a66dc3bbb7
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-11-13 13:53:22 +0530

    Update HazelcastConfig.java

--- src/main/java/com/jijo/jerseyhouse/config/hazelcast/HazelcastConfig.java
+++ src/main/java/com/jijo/jerseyhouse/config/hazelcast/HazelcastConfig.java
@@ -4,7 +4,9 @@ import com.hazelcast.config.*;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!junit")
 @Configuration
 @EnableCaching
 public class HazelcastConfig {
