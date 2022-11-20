170930324c3c1feb87543545b95091d5c46b6821
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-11-21 00:01:59 +0530

    Update JerseyRepository.java

--- src/main/java/com/jijo/jerseyhouse/repository/JerseyRepository.java
+++ src/main/java/com/jijo/jerseyhouse/repository/JerseyRepository.java
@@ -1,9 +1,22 @@
 package com.jijo.jerseyhouse.repository;
 
 import com.jijo.jerseyhouse.model.Jersey;
+import com.jijo.jerseyhouse.model.requests.JerseyRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface JerseyRepository extends JpaRepository<Jersey, Integer> {
+    @Query("select j " +
+            "from Jersey j " +
+            "where j.teamCode in ?#{#jerseyRequest.teams} " +
+            "and j.size in ?#{#jerseyRequest.size} " +
+            "and j.seasonCode in ?#{#jerseyRequest.seasons} " +
+            "group by j.seasonCode, j.teamCode " +
+            "order by j.teamCode.teamId")
+    List<Jersey> findJerseyView(@Param("jerseyRequest") JerseyRequest jerseyRequest);
 }
