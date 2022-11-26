d67a23e25efdf3fbf687533d3ae7133f08229946
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-11-26 18:55:20 +0530

    Update JerseyRepository.java

--- src/main/java/com/jijo/jerseyhouse/repository/JerseyRepository.java
+++ src/main/java/com/jijo/jerseyhouse/repository/JerseyRepository.java
@@ -1,7 +1,7 @@
 package com.jijo.jerseyhouse.repository;
 
 import com.jijo.jerseyhouse.model.Jersey;
-import com.jijo.jerseyhouse.model.requests.JerseyRequest;
+import com.jijo.jerseyhouse.dto.JerseyRequestDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,12 +11,13 @@ import java.util.List;
 
 @Repository
 public interface JerseyRepository extends JpaRepository<Jersey, Integer> {
-    @Query("select j " +
+    @Query("select j.teamCode.teamId , j.teamCode.teamName, j.seasonCode.seasonCode, j.seasonCode.startYear, j.seasonCode.endYear, " +
+            "sum(j.stock), coalesce(j.imageUrl, '') " +
             "from Jersey j " +
             "where ((?#{#jerseyRequest.teams}) is null or j.teamCode.teamId in (?#{#jerseyRequest.teams})) " +
             "and ((?#{#jerseyRequest.size}) is null or j.size in (?#{#jerseyRequest.size})) " +
             "and ((?#{#jerseyRequest.seasons}) is null or j.seasonCode.seasonCode in (?#{#jerseyRequest.seasons})) " +
-//            "group by j.seasonCode, j.teamCode " +
+            "group by j.seasonCode, j.teamCode.teamId, j.teamCode.teamName " +
             "order by j.teamCode.teamId")
-    List<Jersey> findJerseyView(@Param("jerseyRequest") JerseyRequest jerseyRequest);
+    List<Object[]> findJerseyView(@Param("jerseyRequest") JerseyRequestDto jerseyRequestDto);
 }
