8f564280a29365e8c5626c759487c9c247c8eaef
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-12-10 19:07:37 +0530

    Update TeamsControllerInterface.java

--- src/main/java/com/jijo/jerseyhouse/controller/TeamsControllerInterface.java
+++ src/main/java/com/jijo/jerseyhouse/controller/TeamsControllerInterface.java
@@ -1,5 +1,6 @@
 package com.jijo.jerseyhouse.controller;
 
+import com.jijo.jerseyhouse.dto.JerseyOrderDto;
 import com.jijo.jerseyhouse.dto.JerseyViewDto;
 import com.jijo.jerseyhouse.model.Jersey;
 import com.jijo.jerseyhouse.model.Teams;
@@ -7,9 +8,11 @@ import com.jijo.jerseyhouse.dto.JerseyRequestDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("teams")
 public interface TeamsControllerInterface {
@@ -22,4 +25,7 @@ public interface TeamsControllerInterface {
 
     @PostMapping("getJerseyView/grouped")
     ResponseEntity<List<JerseyViewDto>> getJerseyGroupedBySize(@RequestBody JerseyRequestDto jerseyRequestDto);
+
+    @PostMapping("/order")
+    ResponseEntity<Map<String, String>> postJerseyOrder(@RequestBody JerseyOrderDto JerseyOrderDto, @RequestHeader String userId);
 }
