e238a5bdd9c85ed39daf1c25cf576db7dc78ef19
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-12-10 19:07:19 +0530

    Create ProductService.java

---
+++ src/main/java/com/jijo/jerseyhouse/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.jijo.jerseyhouse.service;
+
+import com.jijo.jerseyhouse.dto.JerseyViewDto;
+import com.jijo.jerseyhouse.model.Jersey;
+import com.jijo.jerseyhouse.model.Teams;
+import com.jijo.jerseyhouse.dto.JerseyRequestDto;
+
+import java.util.List;
+public interface ProductService {
+    List<Teams> getTeamsByLeagues(List<Integer> leagueCodeList);
+
+    List<Jersey> getJerseyView(JerseyRequestDto jerseyRequestDto);
+
+    List<JerseyViewDto> getJerseyViewGrouped(JerseyRequestDto jerseyRequestDto);
+}
