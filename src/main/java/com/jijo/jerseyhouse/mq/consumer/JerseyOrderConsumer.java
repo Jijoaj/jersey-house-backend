a1057c1bb21580959032efe48fd19bf16768196c
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-12-11 16:41:07 +0530

    Create JerseyOrderConsumer.java

---
+++ src/main/java/com/jijo/jerseyhouse/mq/consumer/JerseyOrderConsumer.java
@@ -0,0 +1,19 @@
+package com.jijo.jerseyhouse.mq.consumer;
+
+import com.jijo.jerseyhouse.dto.JerseyOrderPlacementDto;
+import com.jijo.jerseyhouse.utilities.constants.QueueConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class JerseyOrderConsumer {
+    @RabbitListener(queues = QueueConstants.orderQueueName)
+    public void consumeJerseyOrderFromQueue(JerseyOrderPlacementDto jerseyOrderPlacementDto) throws InterruptedException {
+        log.info("Order received : {} from user : {}", jerseyOrderPlacementDto, jerseyOrderPlacementDto.getUserId());
+        Thread.sleep(5000);
+        // TODO save order
+        log.info("Order saved");
+    }
+}
