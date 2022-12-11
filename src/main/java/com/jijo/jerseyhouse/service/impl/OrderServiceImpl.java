9673607a3dac8d9af5537f0d906051d22a261fbc
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-12-11 16:41:14 +0530

    Update OrderServiceImpl.java

--- src/main/java/com/jijo/jerseyhouse/service/impl/OrderServiceImpl.java
+++ src/main/java/com/jijo/jerseyhouse/service/impl/OrderServiceImpl.java
@@ -3,9 +3,11 @@ package com.jijo.jerseyhouse.service.impl;
 import com.jijo.jerseyhouse.dto.JerseyOrderDto;
 import com.jijo.jerseyhouse.dto.JerseyOrderPlacementDto;
 import com.jijo.jerseyhouse.exception.CommonInternalException;
+import com.jijo.jerseyhouse.mq.publisher.JerseyOrderPublisher;
 import com.jijo.jerseyhouse.service.OrderService;
 import com.jijo.jerseyhouse.utilities.CommonMethods;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
@@ -14,6 +16,13 @@ import java.util.Map;
 @Service
 @Slf4j
 public class OrderServiceImpl implements OrderService {
+
+    private final JerseyOrderPublisher jerseyOrderPublisher;
+
+    public OrderServiceImpl(JerseyOrderPublisher jerseyOrderPublisher) {
+        this.jerseyOrderPublisher = jerseyOrderPublisher;
+    }
+
     /**
      * @param jerseyOrderDto
      * @param userId
@@ -24,10 +33,10 @@ public class OrderServiceImpl implements OrderService {
         // TODO send order to rabbitMQ
         try {
             JerseyOrderPlacementDto newOrder = new JerseyOrderPlacementDto(jerseyOrderDto, userId);
+            return jerseyOrderPublisher.publishOrder(newOrder);
         }catch (Exception e) {
             log.error("Error while creating order for user {} , time {}, error : {}", userId, Calendar.getInstance().getTime(), e);
             throw new CommonInternalException("order creation failed");
         }
-        return CommonMethods.getPendingMapResponse();
     }
 }
