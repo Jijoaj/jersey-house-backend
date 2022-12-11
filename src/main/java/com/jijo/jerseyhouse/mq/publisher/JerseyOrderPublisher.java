ed742ef064d449c0478e7ca2dbd3cc6a7f037cd1
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-12-11 16:42:06 +0530

    Update JerseyOrderPublisher.java

--- src/main/java/com/jijo/jerseyhouse/mq/publisher/JerseyOrderPublisher.java
+++ src/main/java/com/jijo/jerseyhouse/mq/publisher/JerseyOrderPublisher.java
@@ -1,7 +1,6 @@
 package com.jijo.jerseyhouse.mq.publisher;
 
 import com.jijo.jerseyhouse.dto.JerseyOrderPlacementDto;
-import com.jijo.jerseyhouse.dto.OrderStatusDto;
 import com.jijo.jerseyhouse.exception.CommonInternalException;
 
 import java.util.Map;
