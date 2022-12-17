1ce0cbce7e8a51e756141e49bdf389e2d00c2853
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-12-17 17:27:27 +0530

    Update OrderTransformer.java

--- src/main/java/com/jijo/jerseyhouse/transformer/OrderTransformer.java
+++ src/main/java/com/jijo/jerseyhouse/transformer/OrderTransformer.java
@@ -2,13 +2,14 @@ package com.jijo.jerseyhouse.transformer;
 
 import com.jijo.jerseyhouse.dto.JerseyOrderPlacementDto;
 import com.jijo.jerseyhouse.model.OrderPlacement;
+import com.jijo.jerseyhouse.model.enums.OrderStatus;
 import org.springframework.util.Base64Utils;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class OrderTransformer {
-    public static OrderPlacement toOrderPlacement(JerseyOrderPlacementDto jerseyOrderPlacementDto) {
+    public static OrderPlacement createOrder(JerseyOrderPlacementDto jerseyOrderPlacementDto) {
         return OrderPlacement.builder()
                 .orderId(UUID.randomUUID().toString())
                 .teamId(jerseyOrderPlacementDto.getTeamId())
@@ -17,6 +18,7 @@ public class OrderTransformer {
                 .seasonCode(jerseyOrderPlacementDto.getSeasonCode())
                 .userId(Base64Utils.encodeToString(jerseyOrderPlacementDto.getUserId().getBytes(StandardCharsets.UTF_8)))
                 .quantity(jerseyOrderPlacementDto.getQuantity())
+                .orderStatus(OrderStatus.CREATED.orderStatus)
                 .build();
     }
 }
