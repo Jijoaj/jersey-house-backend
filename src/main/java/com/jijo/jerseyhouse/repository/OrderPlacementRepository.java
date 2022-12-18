bc58591e27baebd8a847ada77da86455b950f23f
Jijo A J <60283946+Jijoaj@users.noreply.github.com>
2022-12-18 16:57:17 +0530

    Update OrderPlacementRepository.java

--- src/main/java/com/jijo/jerseyhouse/repository/OrderPlacementRepository.java
+++ src/main/java/com/jijo/jerseyhouse/repository/OrderPlacementRepository.java
@@ -4,6 +4,9 @@ import com.jijo.jerseyhouse.model.OrderPlacement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderPlacementRepository extends JpaRepository<OrderPlacement, String> {
+    Optional<OrderPlacement> findFirstByOrderStatusOrderByOrderPlacedDate(String orderStatus);
 }
